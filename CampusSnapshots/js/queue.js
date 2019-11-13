5e469437b9266cfd51548195aa2181698116e249
bw-fau <54636199+bw-fau@users.noreply.github.com>
2019-11-13 03:52:33 -0500

    Initial work on reporting functionality

--- CampusSnapshots/js/queue.js
+++ CampusSnapshots/js/queue.js
@@ -4,4 +4,20 @@ $(function() {
         $(this).parents('.card').children('.comments').slideToggle(200)
     })
 
+    $('.reportComment').on('click', function(){
+        value = $(this).siblings('.commentID').val()
+        console.log(value)
+    })
+
+    $('.reportPost').on('click', function(){
+        value = $(this).parents('.card').children('.postID').val()
+        console.log(value)
+        $('#reportPostModal').modal()
+    })
+
+    $('#reportPostForm').on('submit', function(e){
+        e.preventDefault()
+        console.log($(this).serialize())
+    })
+
 })
