91c98b52b23bae72ced8984055a4bdfa3118b789
bw-fau <54636199+bw-fau@users.noreply.github.com>
2019-11-16 03:27:08 -0500

    Added report post/comment frontend

--- CampusSnapshots/js/queue.js
+++ CampusSnapshots/js/queue.js
@@ -1,23 +1,106 @@
 $(function() {
     
+    // Toggle comments on button click
     $('.toggleComments').on('click', function(){
         $(this).parents('.card').children('.comments').slideToggle(200)
     })
 
+    // Report comment button event handler
     $('.reportComment').on('click', function(){
         value = $(this).siblings('.commentID').val()
-        console.log(value)
+        modal = createReportCommentModal(value)
+        modal.on('hidden.bs.modal', function() {
+            $(this).remove()
+        })
+        modal.modal()
     })
 
+    // Report post button event handler
     $('.reportPost').on('click', function(){
         value = $(this).parents('.card').children('.postID').val()
-        console.log(value)
-        $('#reportPostModal').modal()
+        modal = createReportPostModal(value)
+        modal.on('hidden.bs.modal', function() {
+            $(this).remove()
+        })
+        modal.modal()
     })
 
-    $('#reportPostForm').on('submit', function(e){
+    // Comment form submission event handler
+    $('#commentForm').on('submit', function(e) {
         e.preventDefault()
+        // TODO: actual submission functionality
         console.log($(this).serialize())
+        $(this)[0].reset()
     })
+
+    // Report post modal form submission event handler
+    $(document).on('submit', '#reportPostForm', function(e) {
+        e.preventDefault()
+        // TODO: actual submission functionality
+        console.log($(this).serialize())
+        $(this).parents('.modal').modal('hide')
+    })
+
+    // Report comment modal form submission event handler
+    $(document).on('submit', '#reportCommentForm', function(e) {
+        e.preventDefault()
+        // TODO: actual submission functionality
+        console.log($(this).serialize())
+        $(this).parents('.modal').modal('hide')
+    })
+
+    // Create report post modal with id in hidden input field
+    function createReportPostModal(id) {
+        modal = "<div class='modal' tabindex='-1' role='dialog'>" +
+        "<div class='modal-dialog'>" + 
+            "<div class='modal-content'>" +
+                "<div class='modal-header'>" +
+                    "<h5 class='modal-title'>Report Post</h5>" +
+                    "<button type='button' class='close' data-dismiss='modal'>" +
+                        "<span>&times;</span>" +
+                    "</button>" +
+                "</div>" +
+                "<div class='modal-body'>" +
+                    "<form id='reportPostForm'>" +
+                        "<input type='hidden' name='postID' value='" + id + "'>" +
+                        "<div class='form-group'>" +
+                            "<label for='reportText'>Report Message:</label>" +
+                            "<textarea class='form-control' name='reportText' rows='3'></textarea>" +
+                        "</div>" +
+                        "<button type='submit' class='btn btn-primary'>Submit</button>" +
+                    "</form>" +
+                "</div>" +
+            "</div>" + 
+        "</div>" +
+    "</div>"
+    return $(modal)
+    }
+
+    // Create report comment modal with id in hidden input field
+    function createReportCommentModal(id) {
+        modal = "<div class='modal' tabindex='-1' role='dialog'>" +
+        "<div class='modal-dialog'>" + 
+            "<div class='modal-content'>" +
+                "<div class='modal-header'>" +
+                    "<h5 class='modal-title'>Report Comment</h5>" +
+                    "<button type='button' class='close' data-dismiss='modal'>" +
+                        "<span>&times;</span>" +
+                    "</button>" +
+                "</div>" +
+                "<div class='modal-body'>" +
+                    "<form id='reportCommentForm'>" +
+                        "<input type='hidden' name='commentID' value='" + id + "'>" +
+                        "<div class='form-group'>" +
+                            "<label for='reportText'>Report Message:</label>" +
+                            "<textarea class='form-control' name='reportText' rows='3'></textarea>" +
+                        "</div>" +
+                        "<button type='submit' class='btn btn-primary'>Submit</button>" +
+                    "</form>" +
+                "</div>" +
+            "</div>" + 
+        "</div>" +
+    "</div>"
+    return $(modal)
+    }
 
 })
