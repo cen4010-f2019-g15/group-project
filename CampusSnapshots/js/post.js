4f4c421f3e6236d250e173b7d813a4d01ba7204a
bw-fau <54636199+bw-fau@users.noreply.github.com>
2019-11-18 18:45:56 -0500

    Added type selection and event fields

--- CampusSnapshots/js/post.js
+++ CampusSnapshots/js/post.js
@@ -1,5 +1,39 @@
 $(function() {
 
+    $('#report').change(function() {
+        var state = $('input[name="type"]:checked').val()
+        var startDate = $('#startDate')
+        var startTime = $('#startTime')
+        var endDate = $('#endDate')
+        var endTime = $('#endTime')
+
+        if (state === 'report') {
+            console.log(state)
+            $('#eventFields').hide(200)
+            startDate.prop('required', false)
+            startTime.prop('required', false)
+            endDate.prop('required', false)
+            endTime.prop('required', false)
+        }
+    })
+
+    $('#event').change(function() {
+        var state = $('input[name="type"]:checked').val()
+        var startDate = $('#startDate')
+        var startTime = $('#startTime')
+        var endDate = $('#endDate')
+        var endTime = $('#endTime')
+
+        if (state === 'event') {
+            console.log(state)
+            $('#eventFields').show(200)
+            startDate.prop('required', true)
+            startTime.prop('required', true)
+            endDate.prop('required', true)
+            endTime.prop('required', true)
+        }
+    })
+
     // Form submission event handler
     $('form').submit(function(e) {
         e.preventDefault()
