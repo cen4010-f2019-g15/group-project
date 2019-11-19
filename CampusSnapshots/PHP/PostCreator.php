a034d8184d79ca8485c094540124d56af81c1d47
coreyjones2016 <55176517+coreyjones2016@users.noreply.github.com>
2019-11-18 20:40:27 -0500

    Update PostCreator.php

--- CampusSnapshots/PHP/PostCreator.php
+++ CampusSnapshots/PHP/PostCreator.php
@@ -1,6 +1,6 @@
 <?php
    require 'PHP/connection.php';
-   if (empty($_POST['title']) == true and empty($_POST['location']) == true and empty($_POST['description']) == true {
+   if (empty($_POST['title']) == true or empty($_POST['location']) == true or empty($_POST['description']) == true {
 	$errorMessage .= <li>"You are missing either a title, location, or description. Please enter this information and try again."</li>
    }
    elseif (empty($_POST['title']) == false and empty($_POST['location']) == false and empty($_POST['description']) == false ) {
