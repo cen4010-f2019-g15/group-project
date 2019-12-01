3d7d79e199f096263425f2e60b036af01dcd0f8f
JBurtn <55117449+JBurtn@users.noreply.github.com>
2019-11-30 22:29:56 -0500

    Update PostCreator.php
    trim and htmlspecialchars a bindParam/bindVale takes it a a strict string. You cannot escape them. shifted out common values

--- CampusSnapshots/PHP/PostCreator.php
+++ CampusSnapshots/PHP/PostCreator.php
@@ -1,40 +1,31 @@
 <?php
    require 'connection.php';
+   require 'fileUpload.php';
+
    if (empty($_POST['title']) == true or empty($_POST['location']) == true or empty($_POST['description'] == true)) {
-	$errorMessage .= "<li>You are missing either a title, location, or description. Please enter this information and try again.</li>";
+		$errorMessage .= "<li>You are missing either a title, location, or description. Please enter this information and try again.</li>";
    }
    elseif (empty($_POST['title']) == false and empty($_POST['location']) == false and empty($_POST['description']) == false ) {
-	   session_start();
-		chdir('../Files/Events/');
-		/*
-		if(!file_exists($_SESSION['UID'])) {
-			mkdir($_SESSION['UID']);
-		}
+	   	session_start();
 		if(!chdir($_SESSION['UID'])) {
 			die("Error changing Directory");
 		}
-		*/
+		
 	  	$posttype = $_POST["type"];
+	   	$title = $_POST['title'];
+	   	$location = $_POST['location'];
+	   	$description = $_POST['description'];
+	   
 	   	$conn = connect();
 		if ($posttype == 'event') {
 
-			$title = $_POST['title'];
-			$location = $_POST['location'];
 			$startdate = $_POST['startDate'];
 			$enddate = $_POST['endDate'];
-			$description = $_POST['description'];
-			$title = htmlspecialchars($title);
-			$location = htmlspecialchars($location);
-			$description = htmlspecialchars($description);
+
 			$addevent = $conn->prepare("INSERT INTO Events VALUES( DEFAULT, ?, ?, DEFAULT, ?, ?, ?, ?)");
 			$addevent->bindParam(1, $_SESSION['UID']);
 			$addevent->bindParam(2, $title);
-			/*
-			if(isset($_FILES["fileToUpload"])){
-				include 'fileUpload.php';
-				$addevent->bindParam(3, $_COOKIE['CurrentFile']); //given null on no picture
-			}
-			*/
+			$addevent->bindParam(3,fileUpload('../Files/Events/' . $_SESSION['UID'] . '/')); //given null on no picture
 			$addevent->bindParam(3, $startdate);
 			$addevent->bindParam(4, $enddate);
 			$addevent->bindParam(5, $location);
@@ -43,25 +34,13 @@
 		}
 		elseif ($posttype == "report") {
 			
-			$title = $_POST['title'];
-			$location = $_POST['location'];
-			$description = $_POST['description'];
+
 			$type = $_POST['reportType']; //if we choose to add in selecting a type, edit this line with the corrent name of the field!
-			$title = htmlspecialchars($title);
-			$location = htmlspecialchars($location);
-			$description = htmlspecialchars($description);
-			$type = trim($type);
-			$type = stripslashes($type);
-			$type = htmlspecialchars($type);
+
 			$addreport = $conn->prepare("INSERT INTO Reports VALUES(DEFAULT, ?, ?, DEFAULT, DEFAULT, ?, ?, ?, ?)");
 			$addreport->bindParam(1, $_SESSION['UID']);
 			$addreport->bindParam(2, $title);
-			/*
-			if(isset($_FILES["fileToUpload"])){
-				include 'fileUpload.php';
-				$login->bindParam(3, $_COOKIE['CurrentFile']);//given null on no picture
-			}
-			*/
+			$addevent->bindParam(3,fileUpload('../Files/Reports/' . $_SESSION['UID'] . '/')); //given null on no picture
 			$addreport->bindParam(3, $type);
 			$addreport->bindValue(4, 1);
 			$addreport->bindParam(5, $location);
