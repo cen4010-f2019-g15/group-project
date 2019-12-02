1e5423a94994e2589f892879e5cb73e26dbccc29
JBurtn <55117449+JBurtn@users.noreply.github.com>
2019-12-02 18:23:09 -0500

    Update login.js

--- CampusSnapshots/js/login.js
+++ CampusSnapshots/js/login.js
@@ -16,6 +16,7 @@ $(function() {
                 var json = JSON.parse(response)
                 if (json['result'] === true) {
                     window.location.href = 'reports.php'
+                    setCookie('Username', formData.get(Username), 1)
                 } else {
                     errorText.text("Invalid Login Information")
                 }
