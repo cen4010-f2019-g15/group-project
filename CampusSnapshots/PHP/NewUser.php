1b185145c9f7c29faf5569b18492970205a28ca6
bw-fau <54636199+bw-fau@users.noreply.github.com>
2019-12-02 18:00:21 -0500

    Had wrond version of file for some reason

--- CampusSnapshots/PHP/NewUser.php
+++ CampusSnapshots/PHP/NewUser.php
@@ -9,7 +9,7 @@ $New_User = $conn->prepare("INSERT INTO Person VALUES
     );
 
 $New_User->bindParam(1, $_POST['username']);
-$New_User->bindParam(2, password_hash($_POST['password']));
+$New_User->bindValue(2, password_hash($_POST['password'], PASSWORD_DEFAULT));
 $New_User->bindParam(3, $_POST['email']);
 $New_User->bindParam(4, $_POST['firstName']);
 $New_User->bindParam(5, $_POST['lastName']);
@@ -20,12 +20,12 @@ if($New_User->errorCode() !== '00000')
     echo 'false';
 else{
     echo 'true';
-    $New_User = $conn->prepare("SELECT UID FROM Person WHERE UserName = ?");
+    $New_User = $conn->prepare("SELECT UID, UserName FROM Person WHERE UserName = ?");
     
     $New_User->bindParam(1, $_POST['username']);
-    $New_User->excecute();
+    $New_User->execute();
 
-    $result = $New_User->fetchColumn();
-    mkdir('../Files/Reports/'. $result);
-    mkdir('../Files/Events/'. $result);
+    $result = $New_User->fetch();
+    echo mkdir('../Files/Reports/' . $result['UID'] . $result['UserName']);
+    echo mkdir('../Files/Events/' . $result['UID'] . $result['UserName']);
 }
